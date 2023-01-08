0dab576454c58ee29ab4ea2b57fc8ec4bd97da0b
asdf37846 <89976244+asdf37846@users.noreply.github.com>
2023-01-09 00:48:41 +0900

    VehicleC++

--- Vehicle/Source/Vehicle/NewVehiclePawn.cpp
+++ Vehicle/Source/Vehicle/NewVehiclePawn.cpp
@@ -2,4 +2,41 @@
 
 
 #include "NewVehiclePawn.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "WheeledVehicleMovementComponent4W.h"
 
+ANewVehiclePawn::ANewVehiclePawn()
+{
+	UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement());
+	
+	// 타이어 최대 부하
+	Vehicle4W->MinNormalizedTireLoad = 0.0f;
+	Vehicle4W->MinNormalizedTireLoadFiltered = 0.2f;
+	Vehicle4W->MaxNormalizedTireLoad = 2.0f;
+	Vehicle4W->MaxNormalizedTireLoadFiltered = 2.0f;
+
+	// 토크 설정
+	Vehicle4W->MaxEngineRPM = 5700.0f;
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->Reset();
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 400.0f);
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(1890.0f, 500.0f);
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(5730.0f, 400.0f);
+
+	// 스티어링 조정
+	Vehicle4W->SteeringCurve.GetRichCurve()->Reset();
+	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(0.0f, 1.0f);
+	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(40.0f, 0.7f);
+	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(120.0f, 0.6f);
+
+	Vehicle4W->DifferentialSetup.DifferentialType = EVehicleDifferential4W::LimitedSlip_4W;
+	Vehicle4W->DifferentialSetup.FrontRearSplit = 0.65;
+	
+	// 자동변속 설정
+	Vehicle4W->TransmissionSetup.bUseGearAutoBox = true;
+	Vehicle4W->TransmissionSetup.GearSwitchTime = 0.1f;
+	Vehicle4W->TransmissionSetup.GearAutoBoxLatency = 1.0f;
+
+}
--- Vehicle/Source/Vehicle/NewVehiclePawn.h
+++ Vehicle/Source/Vehicle/NewVehiclePawn.h
@@ -21,5 +21,28 @@ public:
 
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
+	// 차량 조종
 	void ApplyThrottle(float Val);
+	void ApplySteering(float Val);
+
+	// 시야 조정
+	void LookUp(float Val);
+	void Turn(float Val);
+
+	// 사이드 브레이크
+	void OnHandbrakePressed();
+	void OnHandbrakeReleased();
+
+	// 공중 제어
+	void UpdateInAirControl(float DeltaTime);
+
+protected:
+
+	// 스프링 암
+	UPROPERTY(Category = Camera, EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* SpringArm;
+
+	// 카메라
+	UPROPERTY(Category = Camera, EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* Camera;
 };
